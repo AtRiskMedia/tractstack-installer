962a1b817a28a34450942ecad28eb85755449a59
Adon <a@atriskmedia.com>
2024-04-11 20:14:31 -0400

    added features|sandbox target

--- scripts/tractstack-install.sh
+++ scripts/tractstack-install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-NAME=$1
-SED='s/ZZZZZ/'"$NAME"'/g'
-DRUPAL_OAUTH_CLIENT_SECRET=$(</dev/urandom tr -dc A-Za-z | head -c12)
-SECRET_KEY=$(</dev/urandom tr -dc A-Za-z | head -c22)
-BUILDER_SECRET_KEY=$(</dev/urandom tr -dc A-Za-z | head -c8)
-DB_PASS=$(</dev/urandom tr -dc A-Za-z | head -c12)
-DRUPAL_PASS=$(</dev/urandom tr -dc A-Za-z | head -c22)
-DB_NAME=t8k_"$NAME"
-CONCIERGE_DB_NAME=concierge_"$NAME"
+TARGET=$2
+if [[ -z "$TARGET" ]]; then
+	NAME=$1
+	INSTALL_USER=$1
+elif [[ "$TARGET" == "features" || "$TARGET" == "sandbox" ]]; then
+	NAME="$2"_"$1"
+	INSTALL_USER="t8k"
+else
+	echo To install Tract Stack with into a target environment, please specific features or sandbox
+	echo Usage: sudo ./tractstack-install.sh username target
+	echo ELSE for user install... Usage: sudo ./tractstack-install.sh username
+	echo ""
+	exit 1
+fi
 
 blue='\033[0;34m'
 brightblue='\033[1;34m'
@@ -39,11 +44,26 @@ if [ -d /home/"$NAME" ]; then
 	exit 1
 fi
 
+if [ -d /home/t8k/"$TARGET"/"$NAME" ]; then
+	echo User "$NAME" already exists in "$TARGET".
+	echo ""
+	exit 1
+fi
+
 if [ "$USER" != root ]; then
 	echo Must provide sudo privileges
 	echo ""
 	exit 1
 fi
+
+SED='s/ZZZZZ/'"$NAME"'/g'
+DRUPAL_OAUTH_CLIENT_SECRET=$(</dev/urandom tr -dc A-Za-z | head -c12)
+SECRET_KEY=$(</dev/urandom tr -dc A-Za-z | head -c22)
+BUILDER_SECRET_KEY=$(</dev/urandom tr -dc A-Za-z | head -c8)
+DB_PASS=$(</dev/urandom tr -dc A-Za-z | head -c12)
+DRUPAL_PASS=$(</dev/urandom tr -dc A-Za-z | head -c22)
+DB_NAME=t8k_"$NAME"
+CONCIERGE_DB_NAME=concierge_"$NAME"
 
 echo ""
 echo Creating Drupal database: t8k_"$NAME"
@@ -54,24 +74,44 @@ mysql -e "CREATE USER ${DB_NAME}@localhost IDENTIFIED BY '${DB_PASS}';" >/dev/nu
 mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_NAME}'@'localhost';" >/dev/null 2>&1
 mysql -e "FLUSH PRIVILEGES;" >/dev/null 2>&1
 
-echo ""
-echo Installing Tract Stack as user: "$NAME"
-useradd -m "$NAME"
-mkdir /home/"$NAME"/scripts
-cp ./tractstack-home-init.sh /home/"$NAME"/scripts/
-cp ./tractstack-init-drupal.sh /home/"$NAME"/scripts/
-cp ./build.sh /home/"$NAME"/scripts/
-sudo -H -u "$NAME" bash -c '~/scripts/tractstack-home-init.sh'
-sudo -H -u "$NAME" bash -c '~/scripts/tractstack-init-drupal.sh '"$DB_PASS"' '"$DRUPAL_PASS"
-cp ../files/drupal/web.config /home/"$NAME"/srv/public_html/drupal/oauth_keys
-./fix-drupal.sh /home/"$NAME"/srv/public_html/drupal/web "$NAME"
-chown "$NAME":www-data /home/"$NAME"/srv/public_html/drupal/oauth_keys
-chown "$NAME":www-data /home/"$NAME"/srv/public_html/drupal/oauth_keys/*key
-chown "$NAME":"$NAME" /home/"$NAME"/srv/public_html/drupal/oauth_keys/web.config
-chmod 640 /home/"$NAME"/srv/public_html/drupal/oauth_keys/private.key
-chmod 640 /home/"$NAME"/srv/public_html/drupal/oauth_keys/public.key
-cat ../files/drupal/settings.incl >>/home/"$NAME"/srv/public_html/drupal/web/sites/default/settings.php
-sed -i -e "$SED" /home/"$NAME"/srv/public_html/drupal/web/sites/default/settings.php
+if [ "$USER" == "$INSTALL_USER" ]; then
+	echo ""
+	echo Installing Tract Stack as user: "$NAME"
+	useradd -m "$NAME"
+	mkdir /home/"$NAME"/scripts
+	cp ./tractstack-home-init.sh /home/"$NAME"/scripts/
+	cp ./tractstack-init-drupal.sh /home/"$NAME"/scripts/
+	cp ./build.sh /home/"$NAME"/scripts/
+	sudo -H -u "$NAME" bash -c '~/scripts/tractstack-home-init.sh'
+	sudo -H -u "$NAME" bash -c '~/scripts/tractstack-init-drupal.sh '"$DB_PASS"' '"$DRUPAL_PASS"
+	cp ../files/drupal/web.config /home/"$NAME"/srv/public_html/drupal/oauth_keys
+	./fix-drupal.sh /home/"$NAME"/srv/public_html/drupal/web "$NAME"
+	chown "$NAME":www-data /home/"$NAME"/srv/public_html/drupal/oauth_keys
+	chown "$NAME":www-data /home/"$NAME"/srv/public_html/drupal/oauth_keys/*key
+	chown "$NAME":"$NAME" /home/"$NAME"/srv/public_html/drupal/oauth_keys/web.config
+	chmod 640 /home/"$NAME"/srv/public_html/drupal/oauth_keys/private.key
+	chmod 640 /home/"$NAME"/srv/public_html/drupal/oauth_keys/public.key
+	cat ../files/drupal/settings.incl >>/home/"$NAME"/srv/public_html/drupal/web/sites/default/settings.php
+	sed -i -e "$SED" /home/"$NAME"/srv/public_html/drupal/web/sites/default/settings.php
+else
+	echo ""
+	echo Installing Tract Stack as target: "$TARGET" - >"$NAME"
+	mkdir /home/t8k/"$TARGET"/"$NAME"/scripts
+	cp ./tractstack-home-init.sh /home/t8k/"$TARGET"/"$NAME"/scripts/
+	cp ./tractstack-init-drupal.sh /home/t8k/"$TARGET"/"$NAME"/scripts/
+	cp ./build.sh /home/"t8k/"$TARGET"/$NAME"/scripts/
+	sudo -H -u "$INSTALL_USER" bash -c '~/t8k/"$TARGET"/scripts/tractstack-home-init.sh'
+	sudo -H -u "$INSTALL_USER" bash -c '~/t8k/"$TARGET"/scripts/tractstack-init-drupal.sh '"$DB_PASS"' '"$DRUPAL_PASS"
+	cp ../files/drupal/web.config /home/t8k/"$TARGET"/"$NAME"/srv/public_html/drupal/oauth_keys
+	./fix-drupal.sh /home/t8k/"$TARGET"/"$NAME"/srv/public_html/drupal/web "$NAME"
+	chown t8k:www-data /home/t8k/"$TARGET"/"$NAME"/srv/public_html/drupal/oauth_keys
+	chown t8k:www-data /home/t8k/"$TARGET"/"$NAME"/srv/public_html/drupal/oauth_keys/*key
+	chown t8k:"$NAME" /home/t8k/"$TARGET"/"$NAME"/srv/public_html/drupal/oauth_keys/web.config
+	chmod 640 /home/t8k/"$TARGET"/"$NAME"/srv/public_html/drupal/oauth_keys/private.key
+	chmod 640 /home/t8k/"$TARGET"/"$NAME"/srv/public_html/drupal/oauth_keys/public.key
+	cat ../files/drupal/settings.incl >>/home/t8k/"$TARGET"/"$NAME"/srv/public_html/drupal/web/sites/default/settings.php
+	sed -i -e "$SED" /home/t8k/"$TARGET"/"$NAME"/srv/public_html/drupal/web/sites/default/settings.php
+fi
 
 #echo ""
 #echo Creating a certificate
@@ -123,40 +163,80 @@ sed -i -e "$SED" /etc/systemd/system/t8k-"$NAME".service
 systemctl enable t8k-"$NAME".path
 systemctl start t8k-"$NAME".path
 
-echo ""
-echo Deploying config
-cp ../files/conf/frontend.env.incl /home/"$NAME"/src/gatsby-starter-tractstack/.env.production
-cp ../files/assets/* /home/"$NAME"/src/gatsby-starter-tractstack/assets/
-truncate -s 0 /home/"$NAME"/src/gatsby-starter-tractstack/src/styles/custom.css
-cp /home/"$NAME"/src/gatsby-starter-storykeep/src/custom/codehooks.tsx.example /home/"$NAME"/src/gatsby-starter-storykeep/src/custom/codehooks.tsx
-cp ../files/conf/storykeep.env.incl /home/"$NAME"/src/gatsby-starter-storykeep/.env.production
-cp ../files/assets/* /home/"$NAME"/src/gatsby-starter-storykeep/assets/
-truncate -s 0 /home/"$NAME"/src/gatsby-starter-storykeep/src/styles/custom.css
-cp /home/"$NAME"/src/gatsby-starter-tractstack/src/custom/codehooks.tsx.example /home/"$NAME"/src/gatsby-starter-tractstack/src/custom/codehooks.tsx
-cp ../files/conf/concierge.env.incl /home/"$NAME"/srv/tractstack-concierge/.env
-sed -i -e "$SED" /home/"$NAME"/src/gatsby-starter-tractstack/.env.production
-sed -i -e "$SED" /home/"$NAME"/src/gatsby-starter-storykeep/.env.production
-sed -i -e "$SED" /home/"$NAME"/srv/tractstack-concierge/.env
-touch /home/"$NAME"/src/gatsby-starter-tractstack/tailwind.whitelist
-chown "$NAME":www-data /home/"$NAME"/src/gatsby-starter-tractstack/tailwind.whitelist
-chown "$NAME":www-data /home/"$NAME"/src/gatsby-starter-tractstack/.env.production
-chown "$NAME":www-data /home/"$NAME"/src/gatsby-starter-storykeep/.env.production
-chown "$NAME":www-data /home/"$NAME"/srv/tractstack-concierge/.env
-chown "$NAME":www-data /home/"$NAME"/releases/watch
-chmod 660 /home/"$NAME"/src/gatsby-starter-tractstack/tailwind.whitelist
-chmod 660 /home/"$NAME"/src/gatsby-starter-tractstack/.env.production
-chmod 660 /home/"$NAME"/src/gatsby-starter-storykeep/.env.production
-chmod 660 /home/"$NAME"/srv/tractstack-concierge/.env
-chmod 770 /home/"$NAME"/releases/watch
+if [ "$USER" == "$INSTALL_USER" ]; then
+	echo ""
+	echo Deploying config
+	cp ../files/conf/frontend.env.incl /home/"$NAME"/src/gatsby-starter-tractstack/.env.production
+	cp ../files/assets/* /home/"$NAME"/src/gatsby-starter-tractstack/assets/
+	truncate -s 0 /home/"$NAME"/src/gatsby-starter-tractstack/src/styles/custom.css
+	cp /home/"$NAME"/src/gatsby-starter-storykeep/src/custom/codehooks.tsx.example /home/"$NAME"/src/gatsby-starter-storykeep/src/custom/codehooks.tsx
+	cp ../files/conf/storykeep.env.incl /home/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	cp ../files/assets/* /home/"$NAME"/src/gatsby-starter-storykeep/assets/
+	truncate -s 0 /home/"$NAME"/src/gatsby-starter-storykeep/src/styles/custom.css
+	cp /home/"$NAME"/src/gatsby-starter-tractstack/src/custom/codehooks.tsx.example /home/"$NAME"/src/gatsby-starter-tractstack/src/custom/codehooks.tsx
+	cp ../files/conf/concierge.env.incl /home/"$NAME"/srv/tractstack-concierge/.env
+	sed -i -e "$SED" /home/"$NAME"/src/gatsby-starter-tractstack/.env.production
+	sed -i -e "$SED" /home/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	sed -i -e "$SED" /home/"$NAME"/srv/tractstack-concierge/.env
+	touch /home/"$NAME"/src/gatsby-starter-tractstack/tailwind.whitelist
+	chown "$NAME":www-data /home/"$NAME"/src/gatsby-starter-tractstack/tailwind.whitelist
+	chown "$NAME":www-data /home/"$NAME"/src/gatsby-starter-tractstack/.env.production
+	chown "$NAME":www-data /home/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	chown "$NAME":www-data /home/"$NAME"/srv/tractstack-concierge/.env
+	chown "$NAME":www-data /home/"$NAME"/releases/watch
+	chmod 660 /home/"$NAME"/src/gatsby-starter-tractstack/tailwind.whitelist
+	chmod 660 /home/"$NAME"/src/gatsby-starter-tractstack/.env.production
+	chmod 660 /home/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	chmod 660 /home/"$NAME"/srv/tractstack-concierge/.env
+	chmod 770 /home/"$NAME"/releases/watch
 
-echo BASIC_AUTH_PASSWORD="$DRUPAL_PASS" >>/home/"$NAME"/src/gatsby-starter-tractstack/.env.production
-echo BASIC_AUTH_PASSWORD="$DRUPAL_PASS" >>/home/"$NAME"/src/gatsby-starter-storykeep/.env.production
-echo BUILDER_SECRET_KEY="$BUILDER_SECRET_KEY" >>/home/"$NAME"/src/gatsby-starter-storykeep/.env.production
-echo DRUPAL_OAUTH_CLIENT_SECRET="$DRUPAL_OAUTH_CLIENT_SECRET" >>/home/"$NAME"/src/gatsby-starter-storykeep/.env.production
-echo DB_PASSWORD="$DB_PASS" >>/home/"$NAME"/srv/tractstack-concierge/.env
-echo SECRET_KEY="$SECRET_KEY" >>/home/"$NAME"/srv/tractstack-concierge/.env
-echo BUILDER_SECRET_KEY="$BUILDER_SECRET_KEY" >>/home/"$NAME"/srv/tractstack-concierge/.env
+	echo BASIC_AUTH_PASSWORD="$DRUPAL_PASS" >>/home/"$NAME"/src/gatsby-starter-tractstack/.env.production
+	echo BASIC_AUTH_PASSWORD="$DRUPAL_PASS" >>/home/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	echo BUILDER_SECRET_KEY="$BUILDER_SECRET_KEY" >>/home/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	echo DRUPAL_OAUTH_CLIENT_SECRET="$DRUPAL_OAUTH_CLIENT_SECRET" >>/home/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	echo DB_PASSWORD="$DB_PASS" >>/home/"$NAME"/srv/tractstack-concierge/.env
+	echo SECRET_KEY="$SECRET_KEY" >>/home/"$NAME"/srv/tractstack-concierge/.env
+	echo BUILDER_SECRET_KEY="$BUILDER_SECRET_KEY" >>/home/"$NAME"/srv/tractstack-concierge/.env
 
-echo ""
-echo Running build on storykeep
-sudo -H -u "$NAME" bash -c '~/scripts/build.sh back'
+	echo ""
+	echo Running build on storykeep
+	sudo -H -u "$NAME" bash -c '~/scripts/build.sh back'
+else
+	echo ""
+	echo Deploying config
+	cp ../files/conf/frontend.env.incl /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/.env.production
+	cp ../files/assets/* /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/assets/
+	truncate -s 0 /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/src/styles/custom.css
+	cp /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-storykeep/src/custom/codehooks.tsx.example /home/"$NAME"/src/gatsby-starter-storykeep/src/custom/codehooks.tsx
+	cp ../files/conf/storykeep.env.incl /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	cp ../files/assets/* /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-storykeep/assets/
+	truncate -s 0 /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-storykeep/src/styles/custom.css
+	cp /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/src/custom/codehooks.tsx.example /home/"$NAME"/src/gatsby-starter-tractstack/src/custom/codehooks.tsx
+	cp ../files/conf/concierge.env.incl /home/t8k/"$TARGET"/"$NAME"/srv/tractstack-concierge/.env
+	sed -i -e "$SED" /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/.env.production
+	sed -i -e "$SED" /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	sed -i -e "$SED" /home/t8k/"$TARGET"/"$NAME"/srv/tractstack-concierge/.env
+	touch /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/tailwind.whitelist
+	chown t8k:www-data /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/tailwind.whitelist
+	chown t8k:www-data /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/.env.production
+	chown t8k:www-data /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	chown t8k:www-data /home/t8k/"$TARGET"/"$NAME"/srv/tractstack-concierge/.env
+	chown t8k:www-data /home/t8k/"$TARGET"/"$NAME"/releases/watch
+	chmod 660 /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/tailwind.whitelist
+	chmod 660 /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/.env.production
+	chmod 660 /home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	chmod 660 /home/t8k/"$TARGET"/"$NAME"/srv/tractstack-concierge/.env
+	chmod 770 /home/t8k/"$TARGET"/"$NAME"/releases/watch
+
+	echo BASIC_AUTH_PASSWORD="$DRUPAL_PASS" >>/home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-tractstack/.env.production
+	echo BASIC_AUTH_PASSWORD="$DRUPAL_PASS" >>/home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	echo BUILDER_SECRET_KEY="$BUILDER_SECRET_KEY" >>/home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	echo DRUPAL_OAUTH_CLIENT_SECRET="$DRUPAL_OAUTH_CLIENT_SECRET" >>/home/t8k/"$TARGET"/"$NAME"/src/gatsby-starter-storykeep/.env.production
+	echo DB_PASSWORD="$DB_PASS" >>/home/t8k/"$TARGET"/"$NAME"/srv/tractstack-concierge/.env
+	echo SECRET_KEY="$SECRET_KEY" >>/home/t8k/"$TARGET"/"$NAME"/srv/tractstack-concierge/.env
+	echo BUILDER_SECRET_KEY="$BUILDER_SECRET_KEY" >>/home/t8k/"$TARGET"/"$NAME"/srv/tractstack-concierge/.env
+
+	echo ""
+	echo Running build on storykeep
+	sudo -H -u "$INSTALL_NAME" bash -c '~/t8k/'"$TARGET"'/scripts/build.sh back'
+fi
