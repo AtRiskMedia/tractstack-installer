9640158543294efbf77049d87b89c2a917cdb0dc
Adon <a@atriskmedia.com>
2025-02-11 13:57:23 -0500

    point to main branch!

--- scripts/tractstack-home-init.sh
+++ scripts/tractstack-home-init.sh
@@ -14,9 +14,9 @@ echo Cloning repos.
 cd ~/src/
 echo Installing Tract Stack
 corepack enable
-yes | COREPACK_ENABLE_STRICT=0 corepack prepare pnpm@9.12.3 --activate
+yes | COREPACK_ENABLE_STRICT=0 corepack prepare pnpm@9.15.4 --activate
 pnpm create astro@latest tractstack-storykeep \
-  --template AtRiskMedia/tractstack-starter/template#rc \
+  --template AtRiskMedia/tractstack-starter/template \
   --typescript strict \
   --install \
   --package-manager pnpm \
@@ -27,7 +27,7 @@ echo {} >tractstack-storykeep/config/init.json
 echo {} >tractstack-storykeep/config/turso.json
 cd ~/src/
 echo Installing Code Base for easy updates
-git clone -b rc https://github.com/AtRiskMedia/tractstack-starter.git
+git clone https://github.com/AtRiskMedia/tractstack-starter.git
 cd ~/srv/
 echo Installing your Concierge
 git clone https://github.com/AtRiskMedia/tractstack-concierge.git
