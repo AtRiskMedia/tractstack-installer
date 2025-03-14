d55dcc3433754d6fd096591aa28dc078ccda68e6
Adon <a@atriskmedia.com>
2025-03-14 01:43:56 -0400

    virt env for certbot

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+EMAIL='admin@atriskmedia.com'
 
-certbot --non-interactive --agree-tos --email admin@atriskmedia.com certonly --dns-cloudflare --dns-cloudflare-credentials /root/.secrets/certbot/cloudflare.ini "$@"
+# Activate the virtual environment and run Certbot
+source /home/t8k/certbot_venv/bin/activate
+certbot --non-interactive --agree-tos --email "$EMAIL" certonly --dns-cloudflare --dns-cloudflare-credentials /root/.secrets/certbot/cloudflare.ini "$@"
+deactivate
 
 # $1:
 #  -d example.com -d www.example.com
-
